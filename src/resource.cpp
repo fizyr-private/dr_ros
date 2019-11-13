dd68a17d273a0fb22cb9013da0080f36b0d2e874
Nicolas Slusarenko <n.slusarenko@fizyr.com>
2019-11-13 11:22:25 +0100

    Testing building in BuildBot.

--- src/resource.cpp
+++ src/resource.cpp
@@ -1,13 +1,73 @@
+// This repository
 #include "resource.hpp"
 
+// ROS
 #include <ros/package.h>
 
+// C++
 #include <algorithm>
 #include <stdexcept>
+#include <iostream>
 
 namespace dr {
 
+std::string getPathX(const std::string& package_name) {
+
+	std::string path = ros::package::command("find " + package_name);
+
+	std::cout << "path:" << path << "\n";
+
+	// scrape any newlines out of it
+	for (
+		size_t newline = path.find('\n');
+		newline != std::string::npos;
+		newline = path.find('\n')
+	) {
+		path.erase(newline, 1);
+	}
+
+	return path;
+}
+
+std::string resolveResourceUrl(std::string const & url) {
+	std::string const scheme_seperator = "://";
+	auto scheme_end = std::search(url.begin(), url.end(), scheme_seperator.begin(), scheme_seperator.end());
+
+	// No scheme? Treat the input as a relative file path.
+	if (scheme_end == url.end()) {
+		return url;
+	}
+
+	std::string scheme(url.begin(), scheme_end);
+	auto authority_start = scheme_end + 3;
+	auto path_start      = std::find(authority_start, url.end(), '/');
+
+	std::string authority(authority_start, path_start);
+	std::string path(path_start, url.end());
+
+	std::cout << "scheme:'" << scheme << "'\n";
+	std::cout << "authority:'" << authority << "'\n";
+	std::cout << "path:'" << path << "'\n";
+	std::cout << "=======================\n\n";
+
+	if (scheme == "package") {
+		// std::string package_path = ros::package::getPath(authority);
+		std::string package_path = getPathX(authority);
+		if (package_path.empty()) {
+			throw std::runtime_error("Failed to find package `" + authority + "' for URL: " + url);
+		}
+		return package_path + path;
+
+	} else if (scheme == "file") {
+		if (authority.size()) throw std::runtime_error("Tried to convert non-local file URL to file path: " + url);
+		return path;
+	}
+
+	throw std::runtime_error("Unsupported URL scheme `" + scheme + "' in URL: " + url);
+}
+
 /// Convert a ROS package URL or a regular file URL to a file path.
+/*
 std::string resolveResourceUrl(std::string const & url) {
 	std::string const scheme_seperator = "://";
 	auto scheme_end = std::search(url.begin(), url.end(), scheme_seperator.begin(), scheme_seperator.end());
@@ -34,5 +94,6 @@ std::string resolveResourceUrl(std::string const & url) {
 
 	throw std::runtime_error("Unsupported URL scheme `" + scheme + "' in URL: " + url);
 }
+*/
 
 }
--- test/resource.cpp
+++ test/resource.cpp
@@ -1,13 +1,25 @@
+// This repository
 #include "resource.hpp"
 
+// ROS
 #include <ros/package.h>
 
+// Catch2
 #include <catch2/catch.hpp>
+
+// C++
+#include <iostream>
 
 namespace dr {
 
 TEST_CASE("ResourceTest -- packageUrl", "packageUrl") {
-	REQUIRE(ros::package::getPath("dr_ros") + "/test.file" == resolveResourceUrl("package://dr_ros/test.file"));
+	// REQUIRE(ros::package::getPath("dr_ros") + "/test.file" == resolveResourceUrl("package://dr_ros/test.file"));
+	std::string first = ros::package::getPath("dr_ros") + "/test.file";
+	std::cout << "first:'" << first << "'\n";
+
+	std::string second = resolveResourceUrl("package://dr_ros/test.file");
+	std::cout << "second:'" << second << "'\n";	
+	REQUIRE(first == second);
 }
 
 TEST_CASE("ResourceTest -- localFileUrl", "localFileUrl") {
